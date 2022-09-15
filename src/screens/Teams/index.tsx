138e62e58dbcade7654695bed9daa7faaefae7da
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-09-14 21:59:46 -0300

    create header component

--- src/screens/Teams/index.tsx
+++ src/screens/Teams/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Header } from '@components/Header'
+
 import * as S from './styles'
 
 export function Teams() {
   return (
     <S.Container>
+      <Header />
     </S.Container>
   )
 }
